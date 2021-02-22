fca9924f2217d62e5ef4bed779ba21fa975ff8c7
Daniel Kipkemei <dann.professional@gmail.com>
2021-02-22 07:51:05 +0300

    setup heroku deployment

--- api/settings.py
+++ api/settings.py
@@ -172,3 +172,7 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'data'),
+)
